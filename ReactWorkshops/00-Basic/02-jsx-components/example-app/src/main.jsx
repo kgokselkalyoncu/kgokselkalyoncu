5fac8ef0d65a3ac01b54c9450982b48fc4094638
kgokselkalyoncu <goksel.kalyoncu@gmail.com>
2025-08-25 01:12:19 +0300

    :sparkles: #1 - #3 JSX ProfileCard çalışması

--- ReactWorkshops/00-Basic/02-jsx-components/example-app/src/main.jsx
+++ ReactWorkshops/00-Basic/02-jsx-components/example-app/src/main.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome } from '@fortawesome/free-solid-svg-icons'
+import './assets/css/main-style.css'
 
 function Header(props){
   const header = props.content;
 
   return (
-    <React.Fragment>
+    <>
       <header>{header}</header>
-    </React.Fragment>
+    </>
   )
 }
-
 
 function Footer(props){
   const footer = props.content;
 
   return (
-    <React.Fragment>
+    <>
       <footer>{footer} - {props.date}</footer>
-    </React.Fragment>
+    </>
   )
 }
 
@@ -27,29 +28,80 @@ function Content(props){
   const date = new Date();
 
   return (
-    <React.Fragment>
+    <>
       <Header content="Ana Başlık"/>
-      <h2>{props.title}</h2>
-      <p>{props.content}</p>
+      <h2><FontAwesomeIcon icon={faHome} /> {props.title}</h2>
+      <p><FontAwesomeIcon icon={faEnvelope} />{props.content}</p>
       <span>{props.date}</span>
       <Footer content="&copy; Copyright" date={date.toLocaleString("tr-TR",{timeZone:"UTC"})}/>
-    </React.Fragment>
+    </>
+  )
+}
+
+function Image(props){
+
+  const srcPath = "./images/" + props.src; // profile-picture-3.jpg
+  const alt = props.alt;
+
+  return (
+    <>
+      <img className="w-24 h-24 mb-3 rounded-full shadow-lg" src={srcPath} alt={alt}/>
+    </>
+  )
+}
+
+function Profile(props){
+  return (
+    <>
+      <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{props.name}</h5>
+      <span className="text-sm text-gray-500 dark:text-gray-400">{props.age}</span>
+    </>
+  )
+}
+
+function ProfileItem(props){
+  return (
+    <>
+      <div className="flex flex-col items-center pb-10 px-4 pt-4">
+        <Image src={props.src} alt={props.src}/>
+        <Profile name={props.name} age={props.age} />
+      </div>
+    </>
+  )
+}
+
+function ProfileCard(){
+  return (
+    <>
+      <div key="1" className='w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700'>
+        <ProfileItem name="Neil Sims" age="28" src="profile-picture-1.jpg" alt="Neil image"  />
+      </div>
+      <div key="2" className='w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700'>
+        <ProfileItem name="Michael Gough" age="32" src="profile-picture-2.jpg" alt="Michael image"  />
+      </div>
+      <div key="3" className='w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700'>
+        <ProfileItem name="Bonnie Green" age="27" src="profile-picture-3.jpg" alt="Bonnie image"  />
+      </div>
+    </>
   )
 }
 
 function App(){
   return (
-  <React.Fragment>
-    <h1>Application</h1>
-    <p>Merhaba bu bir App fonksiyon bileşenidir.</p>
+  <>
+    <h1 className='text-3xl font-bold underline'>Application</h1>
+    <p>Merhaba bu bir App fonksiyon bileşenidir...</p>
     <p>Aşağıda gördünüğünüz içerikler iç içe fonksiyon bileşenlerdir.</p>
-    <Content title="İçerik Başlık" content="Bu bir içeriktir" date="12/08/2025 15:00" />
-  </React.Fragment>
+    <Content title="İçerik Başlık Test" content="Bu bir içeriktir" date="12/08/2025 15:00" />
+    <ProfileCard />
+  </>
   )
 }
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+export default App;
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>
 )
